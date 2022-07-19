816f0c2be7aff0520db4d80cb07916eab77e48f6
pstep333 <pstephens333@hotmail.com>
2022-07-19 23:36:24 +0800

    Finally have the javascript working

--- main.js
+++ main.js
@@ -22,28 +22,37 @@ const projs = [
 // Put each project name to link with pictures in the HTML
 const portfolio = [];
 projs.forEach(function(proj){
-    portfolio.push(document.getElementsByClassName(proj.name));
+    portfolio.push(document.getElementById(proj.name));
 })
 
-// Function that changes the color of the picture pressed
-let picClick = (event) => {
-    event.target.style.backgroundColor = 'blue';
-  }
+let Make_Proj_Data = (proj) => {
+    let info = `<h3>Name: </h3> ${proj.name}<br>
+    <h3>Using: </h3> <span class="word" title="using">${proj.using}</span><br>
+    <h3>Date: </h3> <span class="word"  title="date">${proj.date}</span><br>
+    <h3>Description: </h3> <span class="word" title="description">${proj.description}</span><br>`;
+    return info;
+}
 
-// Inject data into the .proj_data area?? How to do this...  
-let picReturn = (event) => {
-    event.target.style.backgroundColor = '';
+
+// Inject data into the .proj_data area?? Is there an easier way for 3 projects...  
+let Proj1 = (event) => {
+    let data = projs[0];
+    document.getElementById('proj_data').innerHTML = Make_Proj_Data(data);
+}
+
+let Proj2 = (event) => {
+    let data = projs[1];
+    document.getElementById('proj_data').innerHTML = Make_Proj_Data(data);
+}
+
+let Proj3 = (event) => {
+    let data = projs[2];
+    document.getElementById('proj_data').innerHTML = Make_Proj_Data(data);
     
-  }
-
-// Press picture with event handler properties
-let picPress = (proj) => {
-    proj.addEventListener('mousedown', picClick);
-    proj.addEventListener('mouseup', picReturn);
 }
 
 // Loop through each project and add an event listener
-portfolio.forEach(function(proj){
-    picPress(proj);
-})
+portfolio[0].onclick = Proj1;
+portfolio[1].onclick = Proj2;
+portfolio[2].onclick = Proj3;
 
